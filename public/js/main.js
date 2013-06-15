61538125c8370697e70a3dc7ed74cecae0ecbc5d
Eric <Ericrius1@gmail.com>
2013-06-15 15:38:46 -0700

    sound stops when user un-hovers over button, and starts over again when they re-hover

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,11 @@ console.log('Hello World');
 $(document).ready(function(){
   var audio = $("#post_clip")[0];
   $("#post_button").mouseenter(function() {
-    debugger;
     audio.play();
+  });
+  $("#post_button").mouseleave(function(){
+    debugger;
+    audio.pause();
+    audio.currentTime = 0;
   });
 });

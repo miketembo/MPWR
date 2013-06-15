a126a9398d657a25e0679bb06f00495ee7d53e36
Bianca <gando.bia@gmail.com>
2013-06-15 15:26:16 -0700

    asdf

---
+++ public/instructions.js
@@ -0,0 +1,18 @@
+
+//ignore me
+// this.button.on('hover', function() {
+//   var $button = $(this.button);
+//   // instructions.video($button);
+//   instructions.voiceover($button);
+// });
+
+
+// var instructions = {
+//   video: function() {
+
+//   },
+
+//   voiceover : function() {
+
+//   }
+// };

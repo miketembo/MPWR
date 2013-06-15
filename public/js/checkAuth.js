75c3812c51635ac8936b25f12ead704419204231
Charles Holbrow <charlesholbrow@gmail.com>
2013-06-15 09:36:15 -0700

    Get Auth Token for user

--- public/js/checkAuth.js
+++ public/js/checkAuth.js
@@ -1,5 +1,11 @@
 if(window.location.hash) {
   var hash = window.location.hash.substring(1);
-  var tokens = hash.split('&');
   console.log(hash);
+  var tokens = {};
+  _(hash.split('&')).each(function(item) {
+    var keyValuePair = item.split('=');
+    if (keyValuePair.length === 2) {
+      tokens[keyValuePair[0]] = keyValuePair[1];
+    }
+  });
 }

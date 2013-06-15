5346b1ab03a4e1a6a5dd90377f5930f93e1abd65
Eric <Ericrius1@gmail.com>
2013-06-15 15:21:04 -0700

    voiceover on rollover

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
 console.log('Hello World');
+
+$(document).ready(function(){
+  var audio = $("#post_clip")[0];
+  $("#post_button").mouseenter(function() {
+    debugger;
+    audio.play();
+  });
+});

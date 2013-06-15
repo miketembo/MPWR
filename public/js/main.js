946562e4e822f017929741cabd136817e92ed89d
Eric <Ericrius1@gmail.com>
2013-06-15 16:14:18 -0700

    buttons now set up to automatically link to audio clips with same id name

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,27 @@
 console.log('Hello World');
 
 $(document).ready(function(){
-  var audio = $("#post_clip")[0];
-  $("#post_button").mouseenter(function() {
-    audio.play();
-  });
-  $("#post_button").mouseleave(function(){
-    debugger;
-    audio.pause();
-    audio.currentTime = 0;
+  // var poust_audio = $("audio#post_clip")[0];
+  // $("button#post_button").mouseenter(function() {
+  //   poust_audio.play();
+  // });
+  // $("button#post_button").mouseleave(function(){
+  //   poust_audio.pause();
+  //   poust_audio.currentTime = 0;
+  // });
+
+  var $buttons = $('button');
+  _.each($buttons, function(button, key, list){
+
+    var audioElement = "audio#" + button.id;
+    var $voiceover_audio = $(audioElement)[0];
+    $(button).on('mouseenter', function(){
+      debugger;
+      $voiceover_audio.play();
+    });
+    $(button).on('mouseleave', function(){
+      $voiceover_audio.pause();
+      $voiceover_audio.currentTime = 0;
+    });
   });
 });

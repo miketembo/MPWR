c98f0dbf0d62b550cf3258dc626cb0d0e0afceae
Bianca <gando.bia@gmail.com>
2013-07-02 17:34:05 -0700

    refactor

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 app.use(express.logger());
 
-console.log('listen on port 3000');
-app.listen(3000);
+console.log('listen on port 8000');
+app.listen(8000);
